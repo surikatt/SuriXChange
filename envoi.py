cc01a0ac383f2c092ae9e3e2d45d78bd3824d4e1
redder34 <ulys.galland@gmail.com>
2023-03-28 12:19:30 +0200

    Modification envoi

--- envoi.py
+++ envoi.py
@@ -3,10 +3,10 @@ import socket
 import uuid
 
 
-host = ''  # Adresse IP du Raspberry Pi récepteur
-port = 8000
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+# host = ''  # Adresse IP du Raspberry Pi récepteur
+# port = 8000
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect((host, port))
 
 def envoyer_message(type_message):
     mac = hex(uuid.getnode())[2:].upper()  
@@ -18,12 +18,12 @@ def envoyer_ouverture():
     envoyer_message('ouverture')
 
 
-"""
+
 from scapy.all import ARP, Ether, srp
 
 # Fonction pour scanner le réseau local et retourner une liste d'adresses IP disponibles
 def scanner_reseau_local():
-    arp = ARP(pdst='192.168.1.1/24')  # Définir la plage d'adresses IP à scanner
+    arp = ARP(pdst='172.16.0.0/16')  # Définir la plage d'adresses IP à scanner
     ether = Ether(dst='ff:ff:ff:ff:ff:ff')
     packet = ether/arp
     result = srp(packet, timeout=3, verbose=0)[0]
@@ -32,10 +32,4 @@ def scanner_reseau_local():
 # Trouver l'adresse IP de l'hôte sur le réseau local et se connecter à lui
 adresses_disponibles = scanner_reseau_local()
 if len(adresses_disponibles) > 0:
-    host = adresses_disponibles[0]  # Utiliser la première adresse IP disponible
-    port = 8000
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
-else:
-    print("Aucun hôte disponible sur le réseau local.")
-"""
+    print(adresses_disponibles)
