7c1d605551f3a0c8980eb69508442c67313168f5
redder34 <ulys.galland@gmail.com>
2023-03-31 15:45:41 +0200

    marche ensemble

--- envoi.py
+++ envoi.py
@@ -1,11 +1,11 @@
 import json
 import socket
 import uuid
-
 from scapy.all import ARP, Ether, IP, srp
-import socket
 
-def recup_adresses_ip():
+port = 8000
+
+def recup_adresses_ip_toute():
     ip_locale = IP(dst="0.0.0.0").src
     # print(ip_local)
     target_ip = f"{ip_locale}/24"
@@ -21,43 +21,31 @@ def recup_adresses_ip():
     
     return liste_ip
 
+def recuperer_connexion():
+    ip_locale = IP(dst="0.0.0.0").src #
+    liste = recup_adresses_ip_toute() #
+    liste.append(ip_locale)           # à retirer sur central
 
-port = 8000
+    for host in liste:
+        try:
+            #print(f"Connexion à {host}...")
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(2)
+            client.connect((host, port))
+            #print(f"Connexion réussie avec l'hôte {host}")
+            print("ggggggggggggggggggggggggg")
+            return client
+        except:
+            pass    # adresse != centrale
 
-ip_locale = IP(dst="0.0.0.0").src
-liste = recup_adresses_ip() 
-liste.append(ip_locale) # à retirer
+client = recuperer_connexion()
+mac = hex(uuid.getnode())[2:].upper()  
 
-for host in liste:
-    try:
-        print(f"Connexion à {host}...")
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.settimeout(2)
-        client.connect((host, port))
-        print(f"Connexion réussie avec l'hôte {host}")
-        message = "Bonjour, Raspberry Pi récepteur!"
-        mac = hex(uuid.getnode())[2:].upper()  # Obtenir l'adresse MAC de l'émetteur
-        data = json.dumps({'mac': mac, 'message': message}).encode('utf-8')
-        client.sendall(data)
-        client.close()
-        break  # Sortir de la boucle si la connexion réussit
-    except:
-        print(f"Impossible de se connecter à l'hôte {host}")
-        
-
-print("Terminé.")
-
-
-"""
-host = ''  # Adresse IP du Raspberry Pi récepteur
-port = 8000
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
-
-def envoyer_message(type_message):
-    mac = hex(uuid.getnode())[2:].upper()  
-    data = json.dumps({'type': type_message, 'mac': mac}).encode('utf-8')
+def envoyer_message(message, autres = {}, ):
+    data = json.dumps({'message': message, 'mac': mac} | autres).encode('utf-8')
     client.sendall(data)
 
-envoyer_message('ouverture')
-"""
+envoyer_message("connexion", {'type_appareil': "camera"})
+envoyer_message("ouverture")
+
+client.close()
--- reception.py
+++ reception.py
@@ -14,31 +14,8 @@ while True:
     conn, addr = server.accept()
     print(f"Connexion acceptée de {addr[0]}:{addr[1]}")
     data = conn.recv(1024)
+    
     if data:
-        message = json.loads(data.decode('utf-8'))['message']
+        message = json.loads(data.decode('utf-8'))
         print(f"Message reçu: {message}")
     conn.close()
-
-"""
-host = ''  # rien = ecoute sur la machine ou le code s'execute
-port = 8000
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen(1)
-
-
-def traiter_message(data):
-    message = json.loads(data.decode('utf-8'))
-    type_message = message['type']
-    print(f"{message}")
-
-
-
-# Boucle principale pour écouter les connexions entrantes et traiter les messages
-while True:
-    conn, addr = server.accept()
-    data = conn.recv(1024)
-    if len(data) != 0:
-        traiter_message(data)
-    conn.close() # a retirer sur serveur
-"""
